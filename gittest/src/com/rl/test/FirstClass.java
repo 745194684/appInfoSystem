5c38ef8c0c0f911a8a6774d7241600ba31f96323
su1 <745194684@qq.com>
2019-06-27 10:23:56 +0800

    git测试

---
+++ gittest/src/com/rl/test/FirstClass.java
@@ -0,0 +1,12 @@
+package com.rl.test;
+
+/**
+ * @author ：suPengCheng
+ * @date ：Created in 19-6-27 10:06
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println(111111);
+        System.out.println("hello");
+    }
+}
